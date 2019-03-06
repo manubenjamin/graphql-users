122f9c97c474487ac7554b8928f1e027152742d9
Manu Benjamin <manu.b@mstack.com>
2019-03-06 23:23:44 +0530

    GraphQL - User schema, Root query created with hardcoded users list and lodash

--- schema/schema.js
+++ schema/schema.js
@@ -1,10 +1,17 @@
 const graphql = require('graphql');
+const _ = require('lodash');
 
 const {
     GraphQLObjectType,
     GraphQLString,
-    GraphQLInt
+    GraphQLInt,
+    GraphQLSchema
 } = graphql;
+
+const users = [
+    { id: '10', firstName: 'Manu', age: 30},
+    { id: '12', firstName: 'Adam', age: 35}
+];
 
 const UserType = new GraphQLObjectType({
     name: 'User',
@@ -14,3 +21,20 @@ const UserType = new GraphQLObjectType({
         age: { type: GraphQLInt }
     }
 });
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        user: {
+            type: UserType,
+            args: {id : { type: GraphQLString } },
+            resolve(parentValue, args) {
+                return _.find(users, { id: args.id });
+            }
+        }
+    }
+})
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+})
